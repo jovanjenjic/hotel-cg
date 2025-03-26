1272b4364ba1a3b6b65009f8af5479a77558ed04
popUpp <mac@Uross-MacBook-Pro.local>
2025-03-26 13:14:42 +0100

    Footer changes and tailwindcss import

--- app/socials/page.jsx
+++ app/socials/page.jsx
@@ -1,29 +1,27 @@
 import Link from "next/link";
+import { FaTripadvisor } from 'react-icons/fa'
 
 const Social = () => {
   return (
     <>
       <ul>
         <li>
-          <Link href="https://www.facebook.com" target="_blank">
+          <Link href="https://www.facebook.com/profile.php?id=61565953885941" target="_blank">
             <i className="fab fa-facebook-f"></i>
           </Link>
         </li>
         <li>
-          <Link href="https://twitter.com" target="_blank">
-            <i className="fab fa-twitter"></i>
+          <Link href="https://www.instagram.com/villa_drobnipijesak?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" target="_blank">
+            <i className="fab fa-instagram"></i>
           </Link>
         </li>
-        <li>
-          <Link href="https://www.behance.net" target="_blank">
-            <i className="fab fa-behance"></i>
-          </Link>
-        </li>
-        <li>
+        {/* <li>
           <Link href="https://www.linkedin.com" target="_blank">
-            <i className="fab fa-linkedin-in"></i>
+            <i className="fab">
+              <FaTripadvisor className="fab"/>
+            </i>
           </Link>
-        </li>
+        </li> */}
       </ul>
     </>
   );
--- components/data/footerOne.js
+++ components/data/footerOne.js
@@ -5,7 +5,6 @@ const footerOne = {
     title_2: 'Other Pages',
     widgetMenus: [
         {link:'/about',title:'About Us'},
-        {link:'/blog-grid',title:'Blog'},
         {link:'/contact',title:'Contact Us'},
         {link:'/room-list',title:'Room List'},
     ],
@@ -13,9 +12,9 @@ const footerOne = {
     title_4: 'Office Info',
     office_des: '1234 Lake Tahoe Blvd, South Lake Tahoe , California USA',
     officeInfos: [
-        {link:'https://www.google.com/maps',info:'GXF4+8HQ Chippenham United Kingdom',icon: <i className="fal fa-map-marked-alt"></i>},
-        {link:'mailto:info.help@gmail.com',info:'info.help@gmail.com',icon: <i className="fal fal fa-envelope-open-text"></i>},
-        {link:'tel:+125(895)658568',info:'+125 (895) 658 568',icon: <i className='fal fa-phone-alt'></i>}
+        {link:'https://maps.app.goo.gl/iH5N6Heggig8jv1v5',info:'Drobni Pijesak, Montenegro',icon: <i className="fal fa-map-marked-alt"></i>},
+        {link:'mailto:info@villadrobnipijesak.me',info:'info@villadrobnipijesak.me',icon: <i className="fal fal fa-envelope-open-text"></i>},
+        {link:'tel:+38269216035',info:'+382 69 216 035',icon: <i className='fal fa-phone-alt'></i>}
     ],
   }
 
